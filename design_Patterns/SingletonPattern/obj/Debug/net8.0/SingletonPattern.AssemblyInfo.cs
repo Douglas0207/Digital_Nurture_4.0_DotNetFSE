3d162988310204b7ef79c6db36e578f769137cb9
Douglas0207 <robinson4jesuschrist@gmail.com>
2025-07-13 18:54:13 +0530

    📦 Committing leftover changes before pulling

--- design_Patterns/SingletonPattern/obj/Debug/net8.0/SingletonPattern.AssemblyInfo.cs
+++ design_Patterns/SingletonPattern/obj/Debug/net8.0/SingletonPattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SingletonPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4515c51d22f59a14947c07685c57756ef35d54e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddfe0ee9b03fd63ab9dde520d11fb2e54ded31a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SingletonPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SingletonPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
